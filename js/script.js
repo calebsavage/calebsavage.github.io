aff819598b004b64f4c07b1b889553d6af48f44c
Caleb Savage <calebsavagephoto@gmail.com>
2015-12-16 14:06:15 -0500

    testing missile command

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 
 "use strict";
 (function () {
+console.log("script.js loaded");
+	var dropMaker = function(){
+		var drop = $("<div class ='raindrop'></div>")
+		$(".game").append(drop);
+
+		drop.css("top", 30);
+		drop.css("left", Math.floor(Math.random() * 1024));
+	}
 	
+	debugger;
 })
 ();
